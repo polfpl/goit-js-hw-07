4e1d2ff13dfa51b663c13dccfa7140efbef436c6
Paweł Firlej <polf@polf.pl>
2024-06-02 19:05:09 +0200

    Zadanie 1
    pliki zadania 1: ex1html.html; ex1js.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # goit-js-hw-07
  Zadania z modułu 7
+
+Zasanie 1 pliki: ex1html.html; ex1js.js
---
+++ ex1js.js
@@ -0,0 +1,11 @@
+// Policzenie i wyświetlenie liczby kategorii
+const categories = document.querySelectorAll('ul#categories li.item');
+console.log(`Number of categories: ${categories.length}`);
+
+// Dla każdej kategorii wyświetlenie tytułu i liczby elementów
+categories.forEach((category) => {
+  const title = category.querySelector('h2').textContent;
+  const items = category.querySelectorAll('ul li');
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${items.length}`)
+});
